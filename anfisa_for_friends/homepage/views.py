90e4110e17abfb69eb2681be142806951ce82cf3
Дмитрий Трапезников <dima_bms@mail.ru>
2023-10-12 23:20:29 +0300

    before_Order_By

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.db.models import Q
 
+from ice_cream.models import IceCream
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    # Возьмём нужное. А ненужное не возьмём:
+    ice_cream_list = IceCream.objects.values('id', 'title', 'description'
+    ).filter(
+        (Q(is_on_main=True) & Q(is_published=True))
+        | (Q(title__contains='пломбир') & Q(is_published=True))
+    )
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template_name, context)
